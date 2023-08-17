bdb73b54e39bc35ee4e75bd9adcdb298627b3359
anassraissi <https://github.com/anassraissi>
2023-08-17 10:14:32 +0100

    fetch data from data.json to courses  component

--- app/Components/Courses.jsx
+++ app/Components/Courses.jsx
@@ -1,13 +1,21 @@
 import Link from 'next/link';
 import React from 'react'
-import cours from '../data.json'
+import course from '../data.json'
 
 const courses = () => {
-    console.log(cours)
 
   return (
-    <div className='courses' >
-        {cours[0].id}
+    <div >
+        {course.map((course) => (
+        <div key={course.id} className='card'>
+          <h2>{course.title}</h2>
+          <small>Level: {course.level}</small>
+          <p>{course.description}</p>
+          <Link href={course.link} target='_blank' className='btn'>
+            Go To Course
+          </Link>
+        </div>
+      ))}
     </div>
   )
 }
